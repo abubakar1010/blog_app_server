d8b7a930344f896b1902091d7599fc8a657fe3b2
abu bakar <abubakar850772@gmail.com>
2024-12-21 17:22:55 +0600

    add validation schemas for blog creation and updates

---
+++ src/app/modules/blog/blog.validation.ts
@@ -0,0 +1,21 @@
+import e from "express";
+import { z } from "zod";
+
+//
+const blogValidationSchema = z.object({
+	body: z.object({
+		title: z.string().nonempty({ message: "title is required" }),
+		content: z.string().nonempty({ message: "content is required" }),
+		isPublished: z.boolean().default(false),
+		author: z.string().nonempty({ message: "author is required" }),
+	}),
+});
+
+const blogUpdateValidationSchema = z.object({
+	body: z.object({
+		title: z.string().optional(),
+		content: z.string().optional(),
+	}),
+});
+
+export { blogValidationSchema, blogUpdateValidationSchema };

e1342a12a392003b1ed0f127e12df09e0ea76961
abu bakar <abubakar850772@gmail.com>
2024-12-21 16:45:03 +0600

    add router integration for API routes in app setup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from 'cookie-parser'
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
+import router from "./app/routes/route";
 export const app = express();
 
 // parser
@@ -14,7 +15,11 @@ app.use(
 app.use(express.json());
 app.use(cookieParser())
 
-app.get("/test", (req, res) => {
+// routes
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
 	res.send("test route");
 });
 

6460a6027c297e5aec30fee1a750bbae192a16f4
abu bakar <abubakar850772@gmail.com>
2024-12-21 20:20:42 +0600

    add(routes): integrate blog and admin routes into main router

--- src/app/routes/route.ts
+++ src/app/routes/route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { userRoutes } from "../modules/user/user.route";
 import { authRoutes } from "../modules/auth/auth.route";
+import { blogRoutes } from "../modules/blog/blog.route";
+import { AdminRoutes } from "../modules/admin/admin.route";
 
 
 const router = Router()
@@ -9,6 +10,14 @@ const routesInfo = [
     {
         path: "/auth",
         route: authRoutes
+    },
+    {
+        path: "/blogs",
+        route: blogRoutes
+    },
+    {
+        path: "/admin",
+        route: AdminRoutes
     }
 ]
 

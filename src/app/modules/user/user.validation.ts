eb97893526d94a6f8f379377d0735a751cf0294f
abu bakar <abubakar850772@gmail.com>
2024-12-21 16:54:02 +0600

    add user validation schema using Zod for registration

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,14 @@
+import e from "express";
+import { z } from "zod";
+
+const userValidationSchema = z.object({
+	body: z.object({
+		name: z.string().nonempty({ message: "name is required" }),
+		email: z.string().email({ message: "email is required" }),
+		password: z.string().nonempty({ message: "password is required" }),
+	}),
+});
+
+export {
+    userValidationSchema
+}

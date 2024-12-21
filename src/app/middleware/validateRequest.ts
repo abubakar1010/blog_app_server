84804733f24cbe687e756cd7c24923ee78eb28a0
abu bakar <abubakar850772@gmail.com>
2024-12-21 16:42:21 +0600

    add validateRequest middleware for Zod schema validation

---
+++ src/app/middleware/validateRequest.ts
@@ -0,0 +1,16 @@
+import { RequestHandler } from "express"
+import { AnyZodObject } from "zod"
+import { asyncHandler } from "../utils/asyncHandler"
+
+const validateRequest = (schema: AnyZodObject) => {
+    const schemaValidator:RequestHandler = asyncHandler(async(req, res, next) => {
+        await schema.parseAsync({
+            body: req.body
+        })
+        next()
+    })
+    return schemaValidator
+}
+
+
+export default validateRequest
